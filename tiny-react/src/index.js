7c6dff4685c9ae55588230b65e5da4d00bdc1be1
AriseMarcher <2543061999@qq.com>
2022-10-08 21:24:20 +0800

    virtualDOM节点对比

--- tiny-react/src/index.js
+++ tiny-react/src/index.js
@@ -20,8 +20,30 @@ const virtualDOM = (
   </div>
 )
 
+const modifyDOM = (
+  <div className="container">
+    <h1>你好 Tiny React</h1>
+    <h2 data-test="test123">(编码必杀技)</h2>
+    <div>
+      嵌套1 <div>嵌套 1.1</div>
+    </div>
+    <h3>(观察: 这个将会被改变)</h3>
+    {2 == 1 && <div>如果2和1相等渲染当前内容</div>}
+    {2 == 2 && <div>2</div>}
+    <span>这是一段被修改的内容内容</span>
+    <button onClick={() => alert("你好！！！")}>点击我</button>
+    <h3>这个将会被删除</h3>
+    2, 3
+    <input type="text" />
+  </div>
+)
+
+setTimeout(() => {
+  TinyReact.render(modifyDOM, root)
+}, 2000)
+
 // 将virtual对象转换成真实Dom对象
-// TinyReact.render(virtualDOM, root)
+TinyReact.render(virtualDOM, root)
 
 function Heart (props) {
   return (
@@ -53,5 +75,5 @@ class Alert extends TinyReact.Component {
 }
 
 // TinyReact.render(<Heart title="hello React" />, root)
-TinyReact.render(<Alert name="张三" age={20} />, root)
+// TinyReact.render(<Alert name="张三" age={20} />, root)
 
