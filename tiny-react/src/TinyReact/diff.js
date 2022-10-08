810e9d7f8932192c7ed7af8dffc191ec9c28168d
AriseMarcher <2543061999@qq.com>
2022-10-08 21:58:34 +0800

    更新DOM元素

--- tiny-react/src/TinyReact/diff.js
+++ tiny-react/src/TinyReact/diff.js
@@ -1,4 +1,5 @@
 import mountElement from './mountElement'
+import updateNodeElement from './updateNodeElement'
 import updateTextNode from './updateTextNode'
 
 export default function diff (virtualDOM, container, oldDOM) {
@@ -12,6 +13,7 @@ export default function diff (virtualDOM, container, oldDOM) {
       updateTextNode(virtualDOM, oldVirtualDOM, oldDOM)
     } else {
       // 更新元素属性
+      updateNodeElement(oldDOM, virtualDOM)
     }
 
     virtualDOM.children.forEach((child, i) => {
