e297dd0740a1efabd46f3bd13f62d500f7201094
AriseMarcher <2543061999@qq.com>
2022-10-11 23:49:36 +0800

    组件中最原始的实现

--- code/react-redux-guide/src/index.js
+++ code/react-redux-guide/src/index.js
@@ -1,10 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createStore } from 'redux'
+
+const initialState = {
+  count: 0
+}
+function reducer (state = initialState, action) {
+  switch(action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1
+      }
+    case 'decrement':
+      return {
+        count: state.count - 1
+      };
+    default: 
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+const increment = { type: 'increment' }
+const decrement = { type: 'decrement' }
+
+function Counter () {
+  return (
+    <div>
+      <button onClick={() => store.dispatch(increment)}>+</button>
+      <span>{store.getState().count}</span>
+      <button onClick={() => store.dispatch(decrement)}>-</button>
+    </div>
+  )
+}
+
+store.subscribe(() => {
+  console.log(store.getState())
+  root.render(
+    <React.StrictMode>
+      <Counter />
+    </React.StrictMode>
+  );
+})
+
+console.log(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
