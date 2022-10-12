ee752911456d0f627d2863dfbe9a34766f1eb46f
AriseMarcher <2543061999@qq.com>
2022-10-12 20:06:12 +0800

    redux-thunk

--- code/react-redux-guide/src/store/index.js
+++ code/react-redux-guide/src/store/index.js
@@ -1,11 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import RootReducer from './reducers/root.reducer'
-import logger from './middleware/logger'
-import test from './middleware/test'
-import thunk from './middleware/thunk'
+import thunk from 'redux-thunk'
+// import logger from './middleware/logger'
+// import test from './middleware/test'
+// import thunk from './middleware/thunk'
 
-export const store = createStore(RootReducer, applyMiddleware(
-  logger,
-  test,
-  thunk
-))
+
+export const store = createStore(RootReducer, applyMiddleware(thunk))
