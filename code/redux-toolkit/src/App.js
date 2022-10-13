140493bfbb17d51937cf5cf42878cd4a9472c6d4
AriseMarcher <2543061999@qq.com>
2022-10-13 22:53:03 +0800

    创建任务切片初始代码

---
+++ code/redux-toolkit/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      hello redux-toolkit
+    </div>
+  );
+}
+
+export default App;
---
+++ code/redux-toolkit/src/Store/todos.slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const TODOS_FEATURE_KEY = 'todos'
+
+const { reducer: TodosReducer, actions } = createSlice({
+  name: TODOS_FEATURE_KEY,
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload)
+    }
+  }
+})
+
+export const { addTodo } = actions
+
+export default TodosReducer
