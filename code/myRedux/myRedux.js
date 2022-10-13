fb9773001b938372a40ad08a2134f11417fd723f
AriseMarcher <2543061999@qq.com>
2022-10-13 20:07:09 +0800

    参数约束

--- code/myRedux/myRedux.js
+++ code/myRedux/myRedux.js
@@ -5,6 +5,11 @@
  * }
  */
 function createStore (reducer, preloadedState) {
+  // 约束 reducer 参数类型
+  if (typeof reducer !== 'function') {
+    throw new Error('reducer必须是函数')
+  }
+
   // store对象中存储的状态
   var currentState = preloadedState
   // 存放订阅者函数
@@ -17,6 +22,15 @@ function createStore (reducer, preloadedState) {
 
   // 触发action
   function dispatch (action) {
+    // 判断 action 是否是对象
+    if (!isPlainObject(action)) {
+      throw new Error('action必须是对象')
+    }
+    // 判断对象中 是否具有 type 属性
+    if (typeof action.type === 'undefined') {
+      throw new Error('action对象中必须要有type属性')
+    }
+
     currentState = reducer(currentState, action)
     // 循环数组 调用订阅者
     for (var i = 0; i < currentListeners.length; i++) {
@@ -38,3 +52,17 @@ function createStore (reducer, preloadedState) {
     subscribe
   }
 }
+
+// 判断 obj 参数是否是对象
+function isPlainObject (obj) {
+  // 排除基本数据类型和null
+  if (typeof obj !== 'object' || obj === null) return false
+  // 区分数组和对象
+  var proto = obj
+  while (Object.getPrototypeOf(proto) != null) {
+    proto = Object.getPrototypeOf(proto)
+  }
+  return Object.getPrototypeOf(obj) === proto
+}
+
+console.log(isPlainObject([]))
