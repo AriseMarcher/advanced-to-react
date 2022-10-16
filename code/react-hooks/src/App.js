1f0fc0818c7d510c7e464e59f474214661fe21a5
AriseMarcher <2543061999@qq.com>
2022-10-16 18:10:57 +0800

    useMemo()

--- code/react-hooks/src/App.js
+++ code/react-hooks/src/App.js
@@ -1,22 +1,19 @@
-import { useEffect, useState } from 'react'
+import { useMemo, useState } from 'react'
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      let result = await getData()
-      console.log(result)
-    })()
-  }, [])
+  const [count, setCount] = useState(0)
+  const [bool, setBool] = useState(true)
+
+  const result = useMemo(() => {
+    return count * 2
+  }, [count])
+
   return <div>
-    app work
+    <span>{count} -- {result} -- {bool ? '真': '假'}</span>
+    <button onClick={() => setCount(count + 1)}>+1</button>
+    <button onClick={() => setBool(!bool)}>+1</button>
+
   </div>
 }
-
-function getData() {
-  return new Promise(resolve => {
-    resolve({msg: 'Hello'})
-  })
-}
-
 
 export default App;
