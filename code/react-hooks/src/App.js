5af80702f82860c15f15e381469a2e9e7c8ca98e
AriseMarcher <2543061999@qq.com>
2022-10-16 18:17:53 +0800

    memo

--- code/react-hooks/src/App.js
+++ code/react-hooks/src/App.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react'
+import { useMemo, useState, memo } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +12,14 @@ function App() {
     <span>{count} -- {result} -- {bool ? '真': '假'}</span>
     <button onClick={() => setCount(count + 1)}>+1</button>
     <button onClick={() => setBool(!bool)}>+1</button>
-
+    <Foo />
   </div>
 }
+
+const Foo = memo(function Foo () {
+  console.log('Foo组件重新渲染了')
+  return <div>Foo组件</div>
+})
+
 
 export default App;
