f8e0020198dff1ac8727be11ada40c7eb83b189a
AriseMarcher <2543061999@qq.com>
2022-10-16 18:05:20 +0800

    useEffect第二个参数

--- code/react-hooks/src/App.js
+++ code/react-hooks/src/App.js
@@ -1,31 +1,21 @@
 import { useEffect, useState } from 'react'
 
 function App() {
-  
-  function onScroll () {
-    console.log('the page is scrolling')
-  }
-
   useEffect(() => {
-    window.addEventListener('scroll', onScroll)
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [])
-
-  const [count, setCount] = useState(0)
-  
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCount(count => count + 1)
-    }, 1000)
-    return () => {
-      clearInterval(timer)
-    }
+    (async () => {
+      let result = await getData()
+      console.log(result)
+    })()
   }, [])
   return <div>
-    <span>{count}</span>
+    app work
   </div>
+}
+
+function getData() {
+  return new Promise(resolve => {
+    resolve({msg: 'Hello'})
+  })
 }
 
 
