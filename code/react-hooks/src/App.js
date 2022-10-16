87e02218c1c319af680b3df7dd7fb65e900cdca5
AriseMarcher <2543061999@qq.com>
2022-10-16 16:58:07 +0800

    useReducer

--- code/react-hooks/src/App.js
+++ code/react-hooks/src/App.js
@@ -1,28 +1,23 @@
-import { useState } from 'react'
+import { useReducer } from 'react'
 
 function App () {
-  // useState 返回 状态数据 设置状态数据的方法
-  // 要以 set进行开头 后面加上相应的名称
-  // const { count, setCount } = useState(0);
-  const [ count, setCount ] = useState(() => 100)
-  console.log(count)
-  console.log(setCount)
-
-  const [ person, setPerson ] = useState({ name: '张三', age: 20 })
-
-  function handleCount () {
-    setCount((count) => {
-      let newCount = count + 100;
-      document.title = newCount;
-      return newCount
-    })
+  function reducer (state, action) {
+    switch (action.type) {
+      case 'increment':
+        return state + 1;
+      case 'decrement':
+        return state - 1;
+      default:
+        return state;
+    }
   }
 
+  const [ count, dispatch ] = useReducer(reducer, 0)
+
   return <div>
-    <span>{count}{person.name}{person.age}</span>
-    <button onClick={() => setCount(count + 1)}>+ 1</button>
-    <button onClick={handleCount}>函数增加</button>
-    <button onClick={() => setPerson({name: '李四', age: 30})}>setPerson</button>
+    <button onClick={() => dispatch({ type: 'decrement'})}>-1</button>
+    <span>{count}</span>
+    <button onClick={() => dispatch({ type: 'increment'})}>+1</button>
   </div>
 }
 
