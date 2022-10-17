cd516cfcd649de4806922685e76fe7659a39e61b
AriseMarcher <2543061999@qq.com>
2022-10-17 20:08:12 +0800

    定制主题

--- code/emotion-guide/src/App.js
+++ code/emotion-guide/src/App.js
@@ -1,28 +1,13 @@
-import { css, keyframes } from '@emotion/react'
+import { css, useTheme } from '@emotion/react'
 
-const move = keyframes`
-  0% {
-    background: skyblue;
-    left: 0;
-    top: 0
-  }
-  100% {
-    background: red;
-    left: 500px;
-    top: 200px;
-  }
-`
-
-const box = css`
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  animation: ${move} 2s ease infinite;
+const primaryColor = props => css`
+  color: ${props.colors.primary}
 `
 
 function App() {
+  const theme = useTheme()
   return (
-    <div css={box}>
+    <div css={theme}>
       Go
     </div>
   );
--- code/emotion-guide/src/index.js
+++ code/emotion-guide/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@emotion/react'
+
+const theme = {
+	colors: {
+		primary: 'tomato'
+	}
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>
 );
 
