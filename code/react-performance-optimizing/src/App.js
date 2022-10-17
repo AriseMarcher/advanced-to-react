fc7d63a503930ef5caf8be779a7ad04e130d7027
AriseMarcher <2543061999@qq.com>
2022-10-17 22:42:58 +0800

    使用占位符优化

--- code/react-performance-optimizing/src/App.js
+++ code/react-performance-optimizing/src/App.js
@@ -1,28 +1,8 @@
-import { lazy, Suspense, useState } from 'react'
-import { BrowserRouter, Switch, Link } from 'react-router-dom'
-
 function App() {
-  let LazyComponent = null
-  const [flag, setFlag] = useState(true)
-
-  if (flag) {
-    LazyComponent = lazy(() => import(/* webpackChunkName "Home" */'./Home'))
-  } else {
-    LazyComponent = lazy(() => import(/* webpackChunkName "List" */'./List'))
-  }
-
-  return <BrowserRouter>
-    <Link to="/">首页</Link>
-    <Link to="/list">列表页面</Link>
-    <button onClick={() => setFlag(flag => !flag)}>简单改变页面</button>
-
-
-    <Switch>
-      <Suspense fallback={<div>loading...</div>}>
-        <LazyComponent />
-      </Suspense>
-    </Switch>
-  </BrowserRouter>
+  return <>
+    <div>message a</div>
+    <div>message a</div>
+  </>
 }
 
 export default App;
