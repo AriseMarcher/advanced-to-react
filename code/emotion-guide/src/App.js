bed6c0e4a9abd77bae4f889db49d5f3f7a330d7e
AriseMarcher <2543061999@qq.com>
2022-10-17 18:08:54 +0800

    css-in-js的使用

--- code/emotion-guide/src/App.js
+++ code/emotion-guide/src/App.js
@@ -1,7 +1,23 @@
+import { css } from '@emotion/react'
+
+const style = css`
+  width: 100px;
+  height: 200px;
+  background: skyblue;
+`
+
+const style1 = css({
+  width: 100,
+  height: 200,
+  background: 'red'
+})
+
+console.log(style1)
+
 function App() {
   return (
-    <div css={{color: 'blue'}}>
-      App work
+    <div css={style1}>
+      App Page
     </div>
   );
 }
