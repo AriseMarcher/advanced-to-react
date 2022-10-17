cd2fca87e40afd5cc9f40d9b0cec73233b5b1df3
AriseMarcher <2543061999@qq.com>
2022-10-17 19:04:54 +0800

    样式化组件props覆盖

--- code/emotion-guide/src/App.js
+++ code/emotion-guide/src/App.js
@@ -3,8 +3,16 @@ import styled from '@emotion/styled'
 const Button = styled.button`
   width: 100px;
   height: 30px;
-  background: #fff;
+  background: ${props => props.bgColor || '#eee'};
 `
+
+// const Container = styled.div(props => ({
+//   width: props.w || 1000,
+//   height: 500,
+//   border: '1px solid #eee',
+//   margin: '0 auto',
+//   background: '#eee',
+// }))
 
 const Container = styled.div({
   width: 1000,
@@ -12,14 +20,15 @@ const Container = styled.div({
   border: '1px solid #eee',
   margin: '0 auto',
   background: '#eee',
-})
+}, props => ({
+  width: props.width
+}))
 
 function App() {
   return (
     <div>
-      <Container>
-        <Button>按钮</Button>
-
+      <Container width={300}>
+        <Button bgColor="blue">按钮</Button>
       </Container>
     </div>
   );
