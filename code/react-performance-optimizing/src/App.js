401358fcda7acfb43cce4618346a174342dc5e15
AriseMarcher <2543061999@qq.com>
2022-10-17 22:49:42 +0800

    内敛函数优化

--- code/react-performance-optimizing/src/App.js
+++ code/react-performance-optimizing/src/App.js
@@ -1,8 +1,24 @@
-function App() {
-  return <>
-    <div>message a</div>
-    <div>message a</div>
-  </>
+import { Component } from 'react'
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      inputValue: ''
+    }
+  }
+
+  setInputValue = e => {
+    this.setState({ inputValue: e.target.value })
+  }
+
+  render () {
+    return (
+      <input
+        value={this.state.inputValue}
+        onChange={this.setInputValue}
+      />
+    )
+  }
 }
 
 export default App;
