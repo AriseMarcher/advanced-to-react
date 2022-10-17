022fb98a8610219adf0c452692e5100a58fd6b62
AriseMarcher <2543061999@qq.com>
2022-10-17 21:15:59 +0800

    shouldComponentDidUpdate

--- code/react-performance-optimizing/src/App.js
+++ code/react-performance-optimizing/src/App.js
@@ -1,19 +1,44 @@
-import { useState } from "react";
-import Test from "./Test";
+import { Component } from 'react'
 
-function App() {
-  const [flag, setFlag] = useState(true)
-  return (
-    <div>
-      App
-      <button onClick={() => setFlag(prev => !prev)}>
-        销毁Test组件
-      </button>
-      {
-        flag && <Test />
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      person: {
+        name: '张三',
+        age: 20,
+        job: 'waiter'
       }
+    }
+  }
+  componentDidMount () {
+    setTimeout(() => {
+      console.log('123')
+      this.setState({
+        person: {
+          ...this.state.person,
+          job: 'teacher'
+        }
+      })
+    })
+  }
+  shouldComponentUpdate (nextProps, nextState) {
+    if (
+      nextState.person.name !== this.state.person.name ||
+      nextState.person.age !== this.state.person.age
+    ) {
+      return true
+    }
+    return false
+  }
+  render () {
+    console.log('this is render')
+    return <div>
+      { this.state.person.name }
+      { this.state.person.age }
     </div>
-  );
+  }
 }
 
-export default App;
+export default App
+ 
