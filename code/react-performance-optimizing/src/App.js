f3e377d209945df09bcd92b942be398986c02394
AriseMarcher <2543061999@qq.com>
2022-10-17 21:26:23 +0800

    memo第二个参数

--- code/react-performance-optimizing/src/App.js
+++ code/react-performance-optimizing/src/App.js
@@ -1,22 +1,37 @@
 import { useEffect, useState, memo } from 'react'
 
-const ShowName = memo(function ({name}) {
+function compare (prevProps, nextProps) {
+  if (
+    prevProps.person.name !== nextProps.person.name ||
+    prevProps.person.age !== nextProps.person.age
+  ) {
+    // false 表示需要重新渲染
+    return false
+  }
+  // true 表示不需要重新渲染 跟 shouldComponentUpdate相反
+  return true
+}
+
+const ShowName = memo(function ({person}) {
   console.log('is Update?')
-  return <div>{name}</div>
-})
+  return <div>{person.name} -- {person.age}</div>
+}, compare)
 
 function App() {
-  const [name] = useState('张三')
-  const [index, setIndex] = useState(0)
+  const [person, setPerson] = useState(({
+    name: '张三',
+    age: 20,
+    job: 'waiter'
+  }))
 
   useEffect(() => {
     setInterval(() => {
-      setIndex(prev => prev + 1)
+      setPerson({...person, job: 'teacher'})
     }, 1000)
   }, [])
 
   return <div>
-    <ShowName name={name} />
+    <ShowName person={person} />
   </div>
 }
 
