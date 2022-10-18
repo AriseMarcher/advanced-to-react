9cf5b6e78b4315ce17fe7984a710ff75d262c05b
AriseMarcher <2543061999@qq.com>
2022-10-18 13:00:47 +0800

    根据环境切换服务端API接口地址

--- code/ecommerce-front/src/index.tsx
+++ code/ecommerce-front/src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from "react-dom"
 import App from './App';
+import { API } from './config';
 const root = document.getElementById('root') as HTMLElement;
+
+console.log(API)
 
 ReactDOM.render(
   <App />,
